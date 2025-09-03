d1ebeffbed44e8e5ab9c9fdec88431310fbc9ffb
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2025-09-03 07:00:43 +0530

    Done: Power of Two

---
+++ PowerofTwo.cpp
@@ -0,0 +1,17 @@
+// Program to check if a number is a power of two
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n > 0 && (n & (n - 1)) == 0) {
+        cout << n << " is a power of two." << endl;
+    } else {
+        cout << n << " is not a power of two." << endl;
+    }
+
+    return 0;
+}
